c4316b91f778232dc56783ee7a6c189eff24c352
Aaron Li <aaron@qokka.ai>
2021-08-16 06:13:28 -0700

    IDefaultReverseResolver

---
+++ contracts/interfaces/IDefaultReverseResolver.sol
@@ -0,0 +1,9 @@
+import "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
+
+interface IDefaultReverseResolver {
+    function name(bytes32) external view returns (string memory);
+
+    function ens() external view returns (ENS);
+
+    function setName(bytes32 node, string memory _name) external;
+}
